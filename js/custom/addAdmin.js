dbde7ec5c6c536d68fe0e78a26e15d89b3cd3a96
Anand Stephan <anandstephan98@gmail.com>
2023-10-06 17:58:27 +0530

    fix:validation of add Admin

--- js/custom/addAdmin.js
+++ js/custom/addAdmin.js
@@ -9,47 +9,28 @@ let password = document.getElementById("password").value
 const emailPattern = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
 const phoneRegex = /^\d{10}$/;
 
-
-// let isEmail = true;
-let isPhone = true;
-
-// if (emailPattern.test(identifier)) {
-//     isEmail = false;
-// //     // console.log("email",isEmail)
-// } 
-
-if (phoneRegex.test(identifier)) {
-    isPhone = false;
-    // console.log("phone",isPhone)
+if(identifier.length===0 || password.length===0){
+    alert("Please Enter the Id/Email or Password")
+}
+else if(areAllCharactersNumbers(identifier)){
+    if (!phoneRegex.test(identifier)) {
+        alert('Please Enter a Valid Phone number')
+    }
+}else if(!emailPattern.test(identifier)){
+    alert("Please Enter a Valid Email Address")
 }
 
-// if (isEmail) {
-//     alert("Not Valid Email Address");
-// } 
-else if (isPhone) {
-    alert("Not Valid Mobile Number");
-}else{
-    alert("hii")
+ else{
+   addAdmin(identifier,password);
 }
-
-
-
-// if (!emailPattern.test(identifier)) {
-//     alert("Enter Proper Email Address");
-//     return;
-// } 
-
-// else if (!phoneRegex.test(identifier)) {
-//     alert("Enter Valid Mobile Number");
-//     return;
-// }
-//  else{
-//    addAdmin(identifier,password);
-// }
 
 
 
 })
+function areAllCharactersNumbers(inputString) {
+    // Use a regular expression to check if all characters are numbers
+    return /^\d+$/.test(inputString);
+}
 
 async function addAdmin(identifier,password){
     const res = await fetch(`http://54.198.229.134:8080/Ajapa_webservice-0.0.1-SNAPSHOT/adminSignup`,{
