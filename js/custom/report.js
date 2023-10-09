82934e69f55d7aac812adb081a7af43ba9ec5a50
anshi08 <sabharwalanshika08@gmail.com>
2023-10-09 15:25:19 -0800

    Done with Report 2

--- js/custom/report.js
+++ js/custom/report.js
@@ -1,6 +1,7 @@
 
 
 async function getTravelReportDateWise(evenId) {
+    console.log(evenId)
     const response = await fetch("http://54.198.229.134:8080/Ajapa_webservice-0.0.1-SNAPSHOT/getTravelReportDateWise/"+evenId,{
         method: "GET",
         headers: {
@@ -21,6 +22,28 @@ async function getTravelReportDateWiseLeavning(evenId) {
     return res;
 }
 
+//Report 2  {reaching date ke acc}
+async function getTravelReportDateWise2(evenId) {
+    const response = await fetch("http://54.198.229.134:8080/Ajapa_webservice-0.0.1-SNAPSHOT/getTravelReportDateWise2/"+evenId,{
+        method: "GET",
+        headers: {
+            "Content-type":"application/json;  charset=UTF-8"
+        }
+    })
+    const res = await response.json()
+    return res;
+}
+//Report 2  {leaving date ke acc}
+async function getTravelReportDateWise2Leaving(evenId) {
+    const response = await fetch("http://54.198.229.134:8080/Ajapa_webservice-0.0.1-SNAPSHOT/getTravelReportDateWise2/"+evenId+"/1",{
+        method: "GET",
+        headers: {
+            "Content-type":"application/json;  charset=UTF-8"
+        }
+    })
+    const res = await response.json()
+    return res;
+}
 
 async function getAllEvents() {
     const response = await fetch("http://54.198.229.134:8080/Ajapa_webservice-0.0.1-SNAPSHOT/getAllEvents",{
@@ -107,10 +130,30 @@ window.addEventListener("DOMContentLoaded",async(e) =>{
 
     })
 
+    report2EventDDL.addEventListener("change",async (e)=>{
+        let dateWiseReports2 = await getTravelReportDateWise2(e.target.value)
+    
+        tableofReport2.innerHTML=null
+        dateWiseReports2.forEach(report => {
+            let tr = document.createElement("tr")
+            tr.innerHTML = `
+            <td>${report.travelDate}</td>
+            <td>${report.totalPersons}</td>
+            <td>${report.totalFamilies}</td>
+            <td>${`12 AM-7 AM<br/>7 AM-12 Noon<br/>12 Noon-5 PM<br/>5 PM-12 AM`}</td>
+            <td>${report.trainPerson}</td>
+            <td>${report.trainNames.map(detail => `${detail .split("-")[1]}<br>`).join('')}</td>
+            <td>${report.flightPerson}</td>
+            <td>${report.roadPerson}</td>
+            `
+            tableofReport2.appendChild(tr)
+        })
+
+    })
+
 
     report3EventDDL.addEventListener("change",async (e)=>{
         let familyWise = await getTravelReportFamilyWise(e.target.value)
-        console.log('hi')
         tableofReport3.innerHTML=null
         familyWise.forEach(report => {
             console.log(report)
@@ -178,6 +221,51 @@ window.addEventListener("DOMContentLoaded",async(e) =>{
         }
     })
 })
+
+//Report 2
+let report2LeavingReaching = document.getElementById("report2LeavingReaching")
+report2LeavingReaching.addEventListener("change",async (e) =>{
+    if(e.target.value === "Leaving Date"){
+        let eventId = document.getElementById("report2Events").options[document.getElementById("report2Events").selectedIndex].value
+       let eventSorted = await getTravelReportDateWise2Leaving(eventId)
+       document.getElementById("report2body").innerHTML=null
+       eventSorted.forEach(report => {
+        let tr = document.createElement("tr")
+        tr.innerHTML = `
+        <td>${report.travelDate}</td>
+        <td>${report.totalPersons}</td>
+        <td>${report.totalFamilies}</td>
+        <td>${`12 AM-7 AM<br/>7 AM-12 Noon<br/>12 Noon-5 PM<br/>5 PM-12 AM`}</td>
+        <td>${report.trainPerson}</td>
+        <td>${report.trainNames.map(detail => `${detail .split("-")[1]}<br>`).join('')}</td>
+        <td>${report.flightPerson}</td>
+        <td>${report.roadPerson}</td>
+        `
+        document.getElementById("report2body").appendChild(tr)
+       })
+    }else{
+        let eventId = document.getElementById("report2Events").options[document.getElementById("report2Events").selectedIndex].value
+       let eventSorted = await getTravelReportDateWise2(eventId)
+       tableofReport2.innerHTML=null
+       eventSorted.forEach(report => {
+        let tr = document.createElement("tr")
+        tr.innerHTML = `
+        <td>${report.travelDate}</td>
+        <td>${report.totalPersons}</td>
+        <td>${report.totalFamilies}</td>
+        <td>${`12 AM-7 AM<br/>7 AM-12 Noon<br/>12 Noon-5 PM<br/>5 PM-12 AM`}</td>
+        <td>${report.trainPerson}</td>
+        <td>${report.trainNames.map(detail => `${detail .split("-")[1]}<br>`).join('')}</td>
+        <td>${report.flightPerson}</td>
+        <td>${report.roadPerson}</td>
+        `
+        tableofReport2.appendChild(tr)
+       })
+    }
+})
+
+
+
 
 document.getElementById("report1export").addEventListener("click",()=>generatePDF(0))
 document.getElementById("report2export").addEventListener("click",()=>generatePDF(1))
