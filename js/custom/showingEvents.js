504b97838b0e10de4873cc6832620657459ed629
anshi08 <sabharwalanshika08@gmail.com>
2023-10-20 16:56:46 -0800

    fix:next and prev in all showing events

--- js/custom/showingEvents.js
+++ js/custom/showingEvents.js
@@ -1,75 +1,196 @@
+let currentPage = 1;
+const eventsPerPage = 5;
 
-
-//pendingDialog101
-if(JSON.parse(localStorage.getItem("role")) === "member" ||JSON.parse(localStorage.getItem("role")) === "head")
-if(JSON.parse(localStorage.getItem("role")) === "admin" || JSON.parse(localStorage.getItem("role")) === "head" 
-|| JSON.parse(localStorage.getItem("role")) === "member"
-){
+//pendingDialog101s
+if(JSON.parse(localStorage.getItem("role")) === "admin" || JSON.parse(localStorage.getItem("role")) === "head" || JSON.parse(localStorage.getItem("role")) === "member"){
     document.getElementById("pendingrequest") !==null ?document.getElementById("pendingrequest").style.display = "none"  :"" 
+    document.getElementById("approvedUserCard") !==null ?document.getElementById("approvedUserCard").style.display = "none"  :"" 
+    document.getElementById("rejectedUserCard") !==null ?document.getElementById("rejectedUserCard").style.display = "none"  :"" 
+    document.getElementById("TotaleventsCard") !==null ?document.getElementById("TotaleventsCard").style.display = "none"  :"" 
 }
+
+
 
 let prev = document.getElementById("prev")
 let next = document.getElementById("next")
 
+prev.style.display = "none"
+window.addEventListener("DOMContentLoaded", async () => {
+
+    
+
+    const role = (JSON.parse(localStorage.getItem("role")))
+    JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role"))==="head"  ? document.getElementById("listedByCol").style.display = 'none' : ''
+  
+    if(role!="admin"){
+        getAge()
+
+    }
+
+    if(role=="admin" || role=="super")
+    getValueForDashboard()
+
+
+            // Create a new spinner
+    const target = document.getElementById('body');
+    const s = new Spinner().spin(target);
+
+    
+    let res 
+    if(role === "admin"){
+        res = await showingOnlyAdminEvents(parseJwt(localStorage.getItem("data")).Identifier)
+    }else{
+        console.log("START")
+        res = await showingAllEvents(1,5)
+    }
+
+
+        if(res.length==0){
+            s.stop();
+        }
+        if(role === "member" || role === "head") {
+            document.getElementById("Book").style.visibility = "visible"
+            document.getElementById("showDetails")!==null ? document.getElementById("showDetails").style.display = "none" :"";
+            // document.getElementById("deleteDetails").style.display = "none";
+            document.getElementById("deleteEventCol")!==null ? document.getElementById("deleteEventCol").style.display = "none" :""
+        }
+        if(role === "super" || role === "admin") {
+            document.getElementById("showDetails")!==null ? document.getElementById("showDetails").style.display = "block" :""
+            // document.getElementById("deleteDetails").style.display = "block";          
+                document.getElementById("Book").style.display = "none";
+    
+
+      
+        }
+        const res1 = await res.filter(res => res.eventStatus!==2)
+        await res1.forEach(data => {
+            let tr = document.createElement("tr")
+        tr.innerHTML = `
+        <td>${data.eventName}</td>
+        <td>${data.eventType}</td>
+        <td>${data.eventLocation}</td>
+        <td>${data.startDate?.split("T")[0]}</td>
+       ${JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role"))==="head"  ? '' :`<td>${data.listedBy.replaceAll("\"","")}</td>`}
+        ${JSON.parse(localStorage.getItem("role")) === "member" ||
+        JSON.parse(localStorage.getItem("role")) === "head"
+        ?`<td><a href='addTravelDetails.html?id=${data.eventId}' class="btn btn-primary">Register</a></td>` : ''}
+        ${JSON.parse(localStorage.getItem("role")) === "super" || data.canModify==="yes" ?document.getElementById('showDetails')!==null ? "<td><a href='showEventsDetails.html?id="+data.eventId+"'>Edit</a></td>" : '':""}
+        ${JSON.parse(localStorage.getItem("role")) === "super" || data.canDelete==="yes" ?document.getElementById('deleteEventCol')!==null ? `<td><a href="#" class="deleteEvent">Delete</a></td>` : JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role")) === "head" ? '' : document.getElementById('deleteEventCol')!==null ? '<td class="deleteEvent"><a href="#">Delete1</a></td>':"":""}
+        <td style="display:none">${data.eventId}</td>
+        `
+        document.getElementById("body").appendChild(tr)
+        s.stop();
+
+        
+    }
+
+         )
+    Array.from(document.getElementsByClassName("deleteEvent")).forEach(item => {
+        item.addEventListener("click",(e)=>{
+            if(e.target.classList.contains("deleteEvent")){
+                if(confirm("Are You sure you want to delete this event")){
+                    let event = e.target.parentElement.nextElementSibling.innerText;
+                    deleteEvent(event)
+                }else{
+                    alert("Sorry")
+                }
+            }
+        })
+    })  
 
 next.addEventListener("click", async () => {
- 
-    let lastChild  = (document.getElementById("body").lastElementChild.lastElementChild.innerHTML)
+    currentPage++;
+    const first = (currentPage - 1) * eventsPerPage + 1;
+    const last = currentPage * eventsPerPage;
+    // let lastChild  = (document.getElementById("body").lastElementChild.lastElementChild.innerHTML)
     const role = (JSON.parse(localStorage.getItem("role")))
     let res 
     if(role === "admin"){
         res = await showingOnlyAdminEvents(parseJwt(localStorage.getItem("data")).Identifier)
     }else{
-        res = await showingAllEvents(lastChild,+lastChild+10)
-        console.log("nextChild",lastChild,+lastChild+10)
-        
+        // res = await showingAllEvents(lastChild,lastChild+10)
+        res = await showingAllEvents(first,last)
     }
+    prev.style.display = "block"
     document.getElementById("body").innerHTML = null
-    res.forEach((data,idx) => {
-        let tr = document.createElement("tr")
+
+    const res1 = await res.filter(res => res.eventStatus!==2)
+    if(res1.length === 0){
+        document.getElementById("body").innerHTML = "<tr><td colspan='7'>No results to display</td></tr>";
+        next.style.display = "none"
+        prev.style.display = "block"
+    }
+    else{
+        await res1.forEach(data => {
+            let tr = document.createElement("tr")
         tr.innerHTML = `
         <td>${data.eventName}</td>
         <td>${data.eventType}</td>
         <td>${data.eventLocation}</td>
         <td>${data.startDate?.split("T")[0]}</td>
-        <td>${data.listedBy}</td>
-        ${JSON.parse(localStorage.getItem("role")) === "super" ?`<td><a href='showEventsDetails.html?id=${data.eventId}'>Edit</a></td>` :''}
+       ${JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role"))==="head"  ? '' :`<td>${data.listedBy.replaceAll("\"","")}</td>`}
+        ${JSON.parse(localStorage.getItem("role")) === "member" ||
+        JSON.parse(localStorage.getItem("role")) === "head"
+        ?`<td><a href='addTravelDetails.html?id=${data.eventId}' class="btn btn-primary">Register</a></td>` : ''}
+        ${JSON.parse(localStorage.getItem("role")) === "super" || data.canModify==="yes" ?document.getElementById('showDetails')!==null ? "<td><a href='showEventsDetails.html?id="+data.eventId+"'>Edit</a></td>" : '':""}
+        ${JSON.parse(localStorage.getItem("role")) === "super" || data.canDelete==="yes" ?document.getElementById('deleteEventCol')!==null ? `<td><a href="#" class="deleteEvent">Delete</a></td>` : JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role")) === "head" ? '' : document.getElementById('deleteEventCol')!==null ? '<td class="deleteEvent"><a href="#">Delete1</a></td>':"":""}
         <td style="display:none">${data.eventId}</td>
         `
         document.getElementById("body").appendChild(tr)
         s.stop();
-    })
+
+        
+    }
+
+         )
+}
+})
 })
 
 prev.addEventListener("click", async () => {
-    let firstChild = (document.getElementById("body").firstElementChild.lastElementChild.innerHTML)
+    if (currentPage > 1) {
+        currentPage--;
+        const first = (currentPage - 1) * eventsPerPage + 1;
+        const last = currentPage * eventsPerPage;
+    // let newFirstChild = (document.getElementById("body").lastElementChild.lastElementChild.innerHTML,6)
+    const role = (JSON.parse(localStorage.getItem("role")))
     let res 
     if(role === "admin"){
         res = await showingOnlyAdminEvents(parseJwt(localStorage.getItem("data")).Identifier)
-    }else{
-        res = await showingAllEvents(lastChild,+lastChild+10)
+    }else {
+        // res = await showingAllEvents(newFirstChild - 5, newFirstChild - 1); 
+        res = await showingAllEvents(first,last)
+        // console.log("previous", newFirstChild - 5, newFirstChild - 1);
     }
+    
+    next.style.display = "block"
+    // prev.style.display = "none"
+
     document.getElementById("body").innerHTML = null
-    res.forEach(data => {
+    const res1 = await res.filter(res => res.eventStatus!==2)
+    await res1.forEach(data => {
         let tr = document.createElement("tr")
-        tr.innerHTML = `
-        <td>${data.eventName}</td>
-        <td>${data.eventType}</td>
-        <td>${data.eventLocation}</td>
-        <td>${data.startDate?.split("T")[0]}</td>
-        <td>${data.listedBy}</td>
-        <td><a href='cards.html?id=${data.eventId}'>Edit</a></td>
-        
-        ${JSON.parse(localStorage.getItem("role")) === "super" ?`<td><a href='showEventsDetails.html?id=${data.eventId}'>Edit</a></td>` :''}
-        <td style="display:none">${data.eventId}</td>
-        `
-        document.getElementById("body").appendChild(tr)
-        
-    })
+    tr.innerHTML = `
+    <td>${data.eventName}</td>
+    <td>${data.eventType}</td>
+    <td>${data.eventLocation}</td>
+    <td>${data.startDate?.split("T")[0]}</td>
+   ${JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role"))==="head"  ? '' :`<td>${data.listedBy.replaceAll("\"","")}</td>`}
+    ${JSON.parse(localStorage.getItem("role")) === "member" ||
+    JSON.parse(localStorage.getItem("role")) === "head"
+    ?`<td><a href='addTravelDetails.html?id=${data.eventId}' class="btn btn-primary">Register</a></td>` : ''}
+    ${JSON.parse(localStorage.getItem("role")) === "super" || data.canModify==="yes" ?document.getElementById('showDetails')!==null ? "<td><a href='showEventsDetails.html?id="+data.eventId+"'>Edit</a></td>" : '':""}
+    ${JSON.parse(localStorage.getItem("role")) === "super" || data.canDelete==="yes" ?document.getElementById('deleteEventCol')!==null ? `<td><a href="#" class="deleteEvent">Delete</a></td>` : JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role")) === "head" ? '' : document.getElementById('deleteEventCol')!==null ? '<td class="deleteEvent"><a href="#">Delete1</a></td>':"":""}
+    <td style="display:none">${data.eventId}</td>
+    `
+    document.getElementById("body").appendChild(tr)
+    s.stop();
+})    
+}
 })
 async function showingAllEvents(first=1,last=25) {
-        console.log(`http://192.168.29.217:8080/getEvents/${first}/${last}`)
-    const response = await fetch(`http://192.168.29.217:8080/getEvents/${first}/${last}`,{
+
+    const response = await fetch(`http://54.198.229.134:8080/Ajapa_webservice-0.0.1-SNAPSHOT/getEvents/${first}/${last}`,{
          method:"GET",
          headers: {
             "Content-type":"application/json;  charset=UTF-8"
@@ -94,83 +215,7 @@ async function showingOnlyAdminEvents(adminId){
    return res;  
 }
 
-window.addEventListener("DOMContentLoaded",async ()=>{
 
-    getAge()
-
-
-    getValueForDashboard()
-
-
-            // Create a new spinner
-    const target = document.getElementById('body');
-    const s = new Spinner().spin(target);
-
-    
-    const role = (JSON.parse(localStorage.getItem("role")))
-    let res 
-    if(role === "admin"){
-        res = await showingOnlyAdminEvents(parseJwt(localStorage.getItem("data")).Identifier)
-    }else{
-        res = await showingAllEvents()
-    }
-
-
-        if(res.length==0){
-            s.stop();
-        }
-        if(role === "member" || role === "head") {
-            document.getElementById("Book").style.visibility = "visible"
-            document.getElementById("showDetails")!==null ? document.getElementById("showDetails").style.display = "none" :"";
-            // document.getElementById("deleteDetails").style.display = "none";
-            document.getElementById("deleteEventCol")!==null ? document.getElementById("deleteEventCol").style.display = "none" :""
-        }
-        if(role === "super" || role === "admin") {
-            document.getElementById("showDetails")!==null ? document.getElementById("showDetails").style.display = "block" :""
-            // document.getElementById("deleteDetails").style.display = "block";          
-                document.getElementById("Book").style.display = "none";
-    
-
-      
-        }
-        const res1 = await res.filter(res => res.eventStatus!==2)
-    
-
-        await res1.forEach(data => {
-            let tr = document.createElement("tr")
-        tr.innerHTML = `
-        <td>${data.eventName}</td>
-        <td>${data.eventType}</td>
-        <td>${data.eventLocation}</td>
-        <td>${data.startDate?.split("T")[0]}</td>
-        <td>${data.listedBy}</td>
-        ${JSON.parse(localStorage.getItem("role")) === "member" ||
-        JSON.parse(localStorage.getItem("role")) === "head"
-        ?`<td><a href='addTravelDetails.html?id=${data.eventId}' class="btn btn-primary">Register</a></td>` : ''}
-        ${JSON.parse(localStorage.getItem("role")) === "super" || data.canModify==="yes" ?document.getElementById('showDetails')!==null ? "<td><a href='showEventsDetails.html?id="+data.eventId+"'>Edit</a></td>" : '':""}
-        ${JSON.parse(localStorage.getItem("role")) === "super" || data.canDelete==="yes" ?document.getElementById('deleteEventCol')!==null ? `<td><a href="#" class="deleteEvent">Delete</a></td>` : JSON.parse(localStorage.getItem("role")) === "member" || JSON.parse(localStorage.getItem("role")) === "head" ? '' : document.getElementById('deleteEventCol')!==null ? '<td class="deleteEvent"><a href="#">Delete1</a></td>':"":""}
-        <td style="display:none">${data.eventId}</td>
-        `
-        document.getElementById("body").appendChild(tr)
-        s.stop();
-
-        
-    }
-
-    )
-    Array.from(document.getElementsByClassName("deleteEvent")).forEach(item => {
-        item.addEventListener("click",(e)=>{
-            if(e.target.classList.contains("deleteEvent")){
-                if(confirm("Are You sure you want to delete this event")){
-                    let event = e.target.parentElement.nextElementSibling.innerText;
-                    deleteEvent(event)
-                }else{
-                    alert("Sorry")
-                }
-            }
-        })
-    })  
-})
 
 
 function getElementByString(str){  
